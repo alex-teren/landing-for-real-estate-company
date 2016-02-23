ca23e37b1f7d271212033b2fdaa59843a4f0dfd7
alex-teren <terentyev.olexandr@gmail.com>
2016-02-23 18:21:36 +0200

    add aside sidebar

--- _html/js/common.js
+++ _html/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
+	$(".box_mnu .panel-heading").each(function() {
+		var ph = $(this).height() + 3;
+		var pdt = $(this).find(".dropdown-toggle");
+		pdt.height(ph)
+	});
+
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
